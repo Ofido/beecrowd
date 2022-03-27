8b0d5a8501a5f4319a417f90098ae08b034a3d78
Ofido <danieldsb98@gmail.com>
2022-03-27 12:34:05 -0300

    wrong folder

--- 1 -INICIANTE/1387 - Og.py
+++
@@ -1,8 +0,0 @@
-#https://www.beecrowd.com.br/judge/pt/problems/view/1387
-
-while True:
-    n, k = list(map(int, input().split(' ')))
-    if n == 0 :
-        break
-    else:
-        print(n + k)
