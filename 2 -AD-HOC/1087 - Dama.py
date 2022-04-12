65eb2f6c57b1f1b0206463c0a2042af28b88f1dd
Ofido <danieldsb98@gmail.com>
2022-04-12 19:06:30 -0300

    new steps

---
+++ 2 -AD-HOC/1087 - Dama.py
@@ -0,0 +1,27 @@
+#https://www.beecrowd.com.br/judge/pt/problems/view/1087
+#
+
+while True:
+    x1, y1, x2, y2 = list(map(int, input().split(' ')))
+
+    if x1 + y1 + x2 + y2 == 0:
+        break
+
+    resolucao = 0
+    # verificando mesma linha
+    if x1 == x2:
+        if y1 == y2:
+            resolucao = 1
+        else:
+            resolucao = 2
+
+    if not resolucao:
+        if y1 == y2:
+            resolucao = 2
+        else:
+            if abs((y2 - y1) / (x2 - x1)) == 1:
+                resolucao = 2
+            else:
+                resolucao = 3
+
+    print(resolucao-1)
---
+++ 2 -AD-HOC/1103 - Alarme Despertador.py
@@ -0,0 +1,19 @@
+#https://www.beecrowd.com.br/judge/pt/problems/view/1103
+#
+
+while True:
+    hora_que_dormiu, minuto_que_dormiu, hora_de_acordar, minuto_de_acordar = list(map(int, input().split(' ')))
+
+    if hora_que_dormiu + minuto_que_dormiu + hora_de_acordar + minuto_de_acordar == 0:
+        break
+
+    tempo_de_sono = 0
+    tempo_que_dormiu = minuto_que_dormiu + hora_que_dormiu*60
+    tempo_de_acordar = minuto_de_acordar + hora_de_acordar*60
+
+    tempo_dormido = tempo_de_acordar - tempo_que_dormiu
+
+    if tempo_dormido <= 0:
+        tempo_dormido += (60 + 23*60)
+
+    print(tempo_dormido)
