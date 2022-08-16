54a12663ce726a8284474269f6b495e1ec632f17
Ofido <danieldsb98@gmail.com>
2022-08-16 14:30:14 -0300

    work on error

--- 1 - INICIANTE/2867 - Digitos.py
+++ 1 - INICIANTE/2867 - Digitos.py
@@ -2,10 +2,10 @@
 
 '''
     Nota: a biblioteca math tem algum erro quando se trata números muito grandes.
-    TODO ainda tem algo ed errado nesse código, ma não consigo identificar o que é ainda.
+    CUIDADO AO TRANSFORMAR NÚMEROS GRANDES EM FLOAT
 '''
 
 qnt = input()
 for _ in range(int(qnt)):
     n, m = list(map(int, input().split()))
-    print(len(f"{(n**m):.0f}"))
+    print(len(f"{(n**m)}"))
